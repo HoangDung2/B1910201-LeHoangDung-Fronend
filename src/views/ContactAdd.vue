2d87c2fe3db1677d96017f6632538652e4a179d0
Le Hoang Dung <hoangdungmxst19@gmail.com>
2023-04-08 07:51:48 +0700

    Khoi tao du an

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,37 @@
+<script>
+import ContactAddForm from '@/components/ContactAddForm.vue'
+import ContactService from '@/services/contact.service'
+
+export default {
+    components: {
+        ContactAddForm
+    },
+    data() {
+        return {
+            message: ''
+        }
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                await ContactService.create(data)
+                this.message = 'Liên hệ được cập nhật thành công'
+            } catch(error) {
+                console.log(error)
+            }
+        }
+    },
+    created() {
+        this.message = ''
+    }
+}
+</script>
+
+<template>
+    <h4>Thêm liên hệ liên hệ</h4>
+    <ContactAddForm 
+        :contact="{}"
+        @submit:contact="addContact"
+    />
+    <p>{{ message }}</p>
+</template>
